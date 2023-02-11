032eccd04907587cfe61d193c5def886e5c1cd97
Stenway <79376889+Stenway@users.noreply.github.com>
2023-02-11 16:03:54 +0100

    Preparations for 0.5.0

--- tests/dev.ts
+++ tests/dev.ts
@@ -1,7 +1,3 @@
 ﻿/* eslint-disable no-console */
-import { SmlDocument } from "../src"
-
-const content = `MyRootElement\nGroup1\nMyFirstAttribute 123\nMySecondAttribute 10 20 30 40 50\nEnd\nMyThirdAttribute "Hello world"\nEnd`
-const document = SmlDocument.parse(content)
 
 console.log(document.toString())
